b06745d3af846674fb537381d06ef79be038ed3a
Abdurrahman <abdurrahman0803@gmail.com>
2023-07-24 00:10:53 +0100

    chore: cleanup

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -62,11 +62,7 @@ const LoginPage = () => {
       <Head>
         <title>Login</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          // crossOrigin="use-credentials"
-        />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Spartan:wght@300;400;500;600&display=swap"
           rel="stylesheet"
